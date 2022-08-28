c8db94d96150d74b81bf2d3922d9e3e7cf8e7238
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-08-28 10:42:18 -0300

    Página da listagem de astros

---
+++ Models/Astro.cs
@@ -0,0 +1,8 @@
+namespace Astromedia_admin.Models;
+public class Astro
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Curiosidades { get; set; }
+    public string Foto { get; set; }
+}
--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Astromedia_admin.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
